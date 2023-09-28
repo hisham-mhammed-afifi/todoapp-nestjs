1952b23cde10e999e43484e40580dbadd8dbe692
hisham.afifi <hisham.mhammed.afifi@gmail.com>
2023-09-28 23:54:17 +0300

    reset everything

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('TODOs app').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, { customCssUrl: './swagger-ui.css', customfavIcon: './favicon-16x16.png', customSwaggerUiPath: './swagger-ui-bundle.js' });
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
