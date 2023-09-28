6a52ee93bafeb32e081e2fb7303781bc5ae8e411
hisham.afifi <hisham.mhammed.afifi@gmail.com>
2023-09-28 19:51:31 +0300

    change th swagger path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,13 @@ import { AppService } from './app.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Root')
-@Controller()
+@Controller('test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @ApiResponse({ status: 200 })
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getTest();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder().setTitle('TODOs app').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document, {});
+  SwaggerModule.setup('', app, document, {});
 
   await app.listen(process.env.PORT);
 }
