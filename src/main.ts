b83d4bed44d3fe7f22c13aacf8720ad8ca04fa7c
hisham.afifi <hisham.mhammed.afifi@gmail.com>
2023-09-28 19:12:22 +0300

    fix deployment issues

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const config = new DocumentBuilder().setBasePath('/').setTitle('TODOs app').setVersion('1.0').build();
+  const config = new DocumentBuilder().setTitle('TODOs app').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document, {});
 
